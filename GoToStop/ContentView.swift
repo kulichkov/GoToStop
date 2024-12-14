ecc8d38cdb85131098fe3dd601d343bf092b63f4
Mikhail Kulichkov <mikhail.kulichkov@gmail.com>
2024-12-14 17:26:57 +0100

    Initial Commit

---
+++ GoToStop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GoToStop
+//
+//  Created by Mikhail Kulichkov on 14.12.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GoToStop/GoToStopApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoToStopApp.swift
+//  GoToStop
+//
+//  Created by Mikhail Kulichkov on 14.12.24.
+//
+
+import SwiftUI
+
+@main
+struct GoToStopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

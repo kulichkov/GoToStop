2a8cf57d856b9b6e6097e16841843e603d3a6c26
Mikhail Kulichkov <mikhail.kulichkov@gmail.com>
2024-12-14 19:48:30 +0100

    Add widget target

---
+++ GoToStopWidget/GoToStopWidgetBundle.swift
@@ -0,0 +1,18 @@
+//
+//  GoToStopWidgetBundle.swift
+//  GoToStopWidget
+//
+//  Created by Mikhail Kulichkov on 14.12.24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct GoToStopWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        GoToStopWidget()
+        GoToStopWidgetControl()
+        GoToStopWidgetLiveActivity()
+    }
+}

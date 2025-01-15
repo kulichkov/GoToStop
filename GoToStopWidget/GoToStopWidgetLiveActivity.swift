b54dfb3d5e828e69791a286acf5e77a6025cd0b2
Mikhail Kulichkov <mikhail.kulichkov@gmail.com>
2025-01-15 12:53:30 +0100

    Change LiveActivity appearance to display trips

--- GoToStopWidget/GoToStopWidgetLiveActivity.swift
+++ GoToStopWidget/GoToStopWidgetLiveActivity.swift
@@ -10,45 +10,94 @@ import WidgetKit
 import SwiftUI
 
 struct GoToStopWidgetAttributes: ActivityAttributes {
+    public struct TripItem: Identifiable, Codable, Hashable {
+        let id: String
+        let name: String
+        let direction: String
+        let minutesLeft: UInt
+    }
+    
     public struct ContentState: Codable, Hashable {
         // Dynamic stateful properties about your activity go here!
-        var emoji: String
+        let trips: [TripItem]
     }
 
     // Fixed non-changing properties about your activity go here!
-    var name: String
+    var stopName: String
 }
 
 struct GoToStopWidgetLiveActivity: Widget {
     var body: some WidgetConfiguration {
         ActivityConfiguration(for: GoToStopWidgetAttributes.self) { context in
+            let stopName = context.attributes.stopName
+            let trips = context.state.trips
+            
             // Lock screen/banner UI goes here
             VStack {
-                Text("Hello \(context.state.emoji)")
+                Text("\(stopName):")
+                ForEach(trips) { trip in
+                    HStack {
+                        Text(trip.name)
+                        Spacer()
+                        Text(trip.direction)
+                        Spacer()
+                        Text("\(trip.minutesLeft)")
+                    }
+                }
             }
+            .font(.caption2)
+            .padding()
             .activityBackgroundTint(Color.cyan)
             .activitySystemActionForegroundColor(Color.black)
 
         } dynamicIsland: { context in
-            DynamicIsland {
+            let stopName = context.attributes.stopName
+            let closestTrip = context.state.trips.first
+            let closestTripName = closestTrip?.name
+            let closestTripShortName = closestTripName?.split(separator: " ").last
+            let closestTripMinutes = closestTrip?.minutesLeft
+            
+            let restTrips = context.state.trips.dropFirst()
+            
+            return DynamicIsland {
                 // Expanded UI goes here.  Compose the expanded UI through
                 // various regions, like leading/trailing/center/bottom
                 DynamicIslandExpandedRegion(.leading) {
-                    Text("Leading")
+                    if let closestTripName {
+                        Text(closestTripName)
+                            .padding(.horizontal)
+                    }
                 }
                 DynamicIslandExpandedRegion(.trailing) {
-                    Text("Trailing")
+                    if let closestTripMinutes {
+                        Text("in \(closestTripMinutes) min")
+                            .padding(.horizontal)
+                    }
                 }
                 DynamicIslandExpandedRegion(.bottom) {
-                    Text("Bottom \(context.state.emoji)")
-                    // more content
+                    VStack {
+                        Text("\(stopName):")
+                        ForEach(restTrips) { trip in
+                            HStack {
+                                Text(trip.name)
+                                Spacer()
+                                Text("in \(trip.minutesLeft) min")
+                            }
+                        }
+                    }
                 }
             } compactLeading: {
-                Text("L")
+                if let closestTripShortName {
+                    Text(closestTripShortName)
+                }
             } compactTrailing: {
-                Text("T \(context.state.emoji)")
+                if let closestTripMinutes {
+                    Text("\(closestTripMinutes)")
+                }
             } minimal: {
-                Text(context.state.emoji)
+                if let trip = context.state.trips.first {
+                    Text("\(trip.minutesLeft)")
+                }
             }
             .widgetURL(URL(string: "http://www.apple.com"))
             .keylineTint(Color.red)
@@ -58,23 +107,63 @@ struct GoToStopWidgetLiveActivity: Widget {
 
 extension GoToStopWidgetAttributes {
     fileprivate static var preview: GoToStopWidgetAttributes {
-        GoToStopWidgetAttributes(name: "World")
+        GoToStopWidgetAttributes(stopName: "F Kuhwaldstraße")
     }
 }
 
 extension GoToStopWidgetAttributes.ContentState {
-    fileprivate static var smiley: GoToStopWidgetAttributes.ContentState {
-        GoToStopWidgetAttributes.ContentState(emoji: "😀")
-     }
-     
-     fileprivate static var starEyes: GoToStopWidgetAttributes.ContentState {
-         GoToStopWidgetAttributes.ContentState(emoji: "🤩")
+    fileprivate static var trams: GoToStopWidgetAttributes.ContentState {
+        GoToStopWidgetAttributes.ContentState(
+            trips: [
+                .init(
+                    id: UUID().uuidString,
+                    name: "Tram 17",
+                    direction: "Frankfurt (Main) Rebstockbad",
+                    minutesLeft: 4
+                ),
+                .init(
+                    id: UUID().uuidString,
+                    name: "Tram 17",
+                    direction: "Frankfurt (Main) Rebstockbad",
+                    minutesLeft: 14
+                ),
+                .init(
+                    id: UUID().uuidString,
+                    name: "Tram 17",
+                    direction: "Frankfurt (Main) Rebstockbad",
+                    minutesLeft: 24
+                ),
+                .init(
+                    id: UUID().uuidString,
+                    name: "Tram 17",
+                    direction: "Frankfurt (Main) Rebstockbad",
+                    minutesLeft: 34
+                ),
+                .init(
+                    id: UUID().uuidString,
+                    name: "Tram 17",
+                    direction: "Frankfurt (Main) Rebstockbad",
+                    minutesLeft: 44
+                ),
+                .init(
+                    id: UUID().uuidString,
+                    name: "Tram 17",
+                    direction: "Frankfurt (Main) Rebstockbad",
+                    minutesLeft: 54
+                ),
+                .init(
+                    id: UUID().uuidString,
+                    name: "Tram 17",
+                    direction: "Frankfurt (Main) Rebstockbad",
+                    minutesLeft: 64
+                ),
+            ]
+        )
      }
 }
 
 #Preview("Notification", as: .content, using: GoToStopWidgetAttributes.preview) {
    GoToStopWidgetLiveActivity()
 } contentStates: {
-    GoToStopWidgetAttributes.ContentState.smiley
-    GoToStopWidgetAttributes.ContentState.starEyes
+    GoToStopWidgetAttributes.ContentState.trams
 }

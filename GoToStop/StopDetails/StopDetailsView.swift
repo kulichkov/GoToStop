70d5f599eb126e13207148c1b421541e4f53e416
Mikhail Kulichkov <mikhail.kulichkov@gmail.com>
2025-02-01 18:50:36 +0100

    Change background color for the app

--- GoToStop/StopDetails/StopDetailsView.swift
+++ GoToStop/StopDetails/StopDetailsView.swift
@@ -11,88 +11,106 @@ struct StopDetailsView: View {
     @StateObject var viewModel: StopScheduleViewModel
     
     var body: some View {
-        ScrollView {
-            VStack(spacing: 20) {
-                // Station Name
-                Text(viewModel.stop.name)
-                    .font(.title)
-                    .fontWeight(.semibold)
-                    .foregroundColor(.primary)
-                    .padding(.top, 20)
-                
-                // Transport List
-                VStack(alignment: .leading, spacing: 12) {
-                    Text("Tracking transport")
-                        .font(.headline)
-                        .foregroundColor(.secondary)
+        ZStack {
+            Color(.systemGray6)
+                .ignoresSafeArea()
+            
+            ScrollView {
+                VStack(spacing: 20) {
+                    // Station Name
+                    Text(viewModel.stop.name)
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.primary)
+                        .padding(.top, 20)
                     
-                    ForEach(viewModel.trips) { tripItem in
-                        HStack(alignment: .top) {
-                            Text("\(tripItem.trip.name) → \(tripItem.trip.direction)")
-                                .font(.subheadline)
-                                .foregroundColor(.primary)
+                    // Transport List
+                    VStack(alignment: .leading, spacing: 12) {
+                        Text("Tracking transport")
+                            .font(.headline)
+                            .foregroundColor(.secondary)
+                        
+                        ForEach(viewModel.trips) { tripItem in
+                            HStack(alignment: .top) {
+                                Text("\(tripItem.trip.name) → \(tripItem.trip.direction)")
+                                    .font(.subheadline)
+                                    .foregroundColor(.primary)
+                                Spacer()
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
+                    .padding(.horizontal)
+                    
+                    // Schedule List
+                    VStack(alignment: .leading, spacing: 12) {
+                        HStack {
+                            Text("Schedule on \(Date.now.formatted(date: .abbreviated, time: .shortened))")
+                                .font(.headline)
+                                .foregroundColor(.secondary)
                             Spacer()
+                        }
+                        
+                        if viewModel.scheduleItems.isEmpty {
+                            HStack {
+                                Text("Loading...")
+                                    .font(.headline)
+                                    .foregroundColor(.secondary)
+                                Spacer()
+                            }
+                        } else {
+                            ForEach(viewModel.scheduleItems) { item in
+                                HStack {
+                                    VStack(alignment: .leading) {
+                                        Text("\(item.name)")
+                                            .font(.body)
+                                            .foregroundColor(.primary)
+                                        
+                                        Text("→ \(item.direction)")
+                                            .font(.body)
+                                            .foregroundColor(.primary)
+                                        
+                                        VStack(alignment: .leading) {
+                                            if let minutesUntilDeparture = item.minutesLeft {
+                                                Text("Departs in \(minutesUntilDeparture) min")
+                                                    .font(.footnote)
+                                                    .foregroundColor(.green)
+                                                    .brightness(-0.2)
+                                            }
+                                            
+                                            if let scheduledTime = item.scheduledTime {
+                                                Text("Scheduled at \(scheduledTime.formatted(date: .omitted, time: .shortened))")
+                                                    .font(.footnote)
+                                                    .foregroundColor(.gray)
+                                            }
+                                            
+                                            if item.timeDiffers, let actualTime = item.realTime {
+                                                Text("Actually at \(actualTime.formatted(date: .omitted, time: .shortened))")
+                                                    .font(.footnote)
+                                                    .foregroundColor(item.isCancelled ? .gray : .orange)
+                                                    .brightness(item.isCancelled ? 1.0 : -0.2)
+                                            }
+                                        }
+                                        .strikethrough(item.isCancelled)
+                                        
+                                        if item.isCancelled {
+                                            Text("Cancelled")
+                                                .font(.footnote)
+                                                .foregroundColor(.red)
+                                        }
+                                    }
+                                    Spacer()
+                                }
+                                .padding(.vertical, 8)
+                                .padding(.horizontal)
+                                .background(Color(UIColor.systemGray5))
+                                .cornerRadius(10)
+                            }
                         }
                     }
                     .padding(.horizontal)
+                    Spacer()
                 }
-                .padding(.horizontal)
-                
-                // Schedule List
-                VStack(alignment: .leading, spacing: 12) {
-                    Text("Schedule on \(Date.now.formatted(date: .abbreviated, time: .shortened))")
-                        .font(.headline)
-                        .foregroundColor(.secondary)
-                    
-                    ForEach(viewModel.scheduleItems) { item in
-                        HStack {
-                            VStack(alignment: .leading) {
-                                Text("\(item.name)")
-                                    .font(.body)
-                                    .foregroundColor(.primary)
-                                
-                                Text("→ \(item.direction)")
-                                    .font(.body)
-                                    .foregroundColor(.primary)
-                                
-                                VStack(alignment: .leading) {
-                                    if let minutesUntilDeparture = item.minutesLeft {
-                                        Text("Departs in \(minutesUntilDeparture) min")
-                                            .font(.footnote)
-                                            .foregroundColor(.green)
-                                    }
-                                    
-                                    if let scheduledTime = item.scheduledTime {
-                                        Text("Scheduled at \(scheduledTime.formatted(date: .omitted, time: .shortened))")
-                                            .font(.footnote)
-                                            .foregroundColor(.gray)
-                                    }
-                                    
-                                    if item.timeDiffers, let actualTime = item.realTime {
-                                        Text("Actually at \(actualTime.formatted(date: .omitted, time: .shortened))")
-                                            .font(.footnote)
-                                            .foregroundColor(item.isCancelled ? .gray : .orange)
-                                    }
-                                }
-                                .strikethrough(item.isCancelled)
-                                
-                                if item.isCancelled {
-                                    Text("Cancelled")
-                                        .font(.footnote)
-                                        .foregroundColor(.red)
-                                }
-                            }
-                            Spacer()
-                        }
-                        .padding(.vertical, 8)
-                        .padding(.horizontal)
-                        .background(Color(UIColor.systemGray6))
-                        .cornerRadius(10)
-                    }
-                }
-                .padding(.horizontal)
-                
-                Spacer()
             }
         }
         .navigationTitle("Stop schedule")
--- GoToStop/WelcomeScreen/WelcomeView.swift
+++ GoToStop/WelcomeScreen/WelcomeView.swift
@@ -9,44 +9,48 @@ import SwiftUI
 
 struct WelcomeView: View {
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Welcome to GoToStop")
-                .font(.largeTitle)
-                .multilineTextAlignment(.center)
-                .fontWeight(.bold)
-            
-            Text("Enhance your experience by adding GoToStop widget")
-                .font(.headline)
-                .foregroundColor(.secondary)
-                .multilineTextAlignment(.center)
-                .padding(.horizontal, 8)
-            
-            VStack(alignment: .leading, spacing: 15) {
-                HStack(spacing: 16) {
-                    Image(systemName: "rectangle.stack.fill.badge.plus")
-                        .foregroundColor(.accentColor)
-                    Text("Add GoToStop widget to your Home Screen")
-                        .font(.body)
-                }
+        ZStack {
+            Color(.systemGray6)
+                .ignoresSafeArea()
+            VStack(spacing: 20) {
+                Text("Welcome to GoToStop")
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
+                    .fontWeight(.bold)
                 
-                HStack(spacing: 16) {
-                    Image(systemName: "wrench.fill")
-                        .foregroundColor(.accentColor)
-                    Text("Setup your widget in Edit Widget")
-                        .font(.body)
-                }
+                Text("Enhance your experience by adding GoToStop widget")
+                    .font(.headline)
+                    .foregroundColor(.secondary)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 8)
                 
-                HStack(spacing: 16) {
-                    Image(systemName: "house.fill")
-                        .foregroundColor(.accentColor)
-                    Text("Stay updated, right from your home screen")
-                        .font(.body)
+                VStack(alignment: .leading, spacing: 15) {
+                    HStack(spacing: 16) {
+                        Image(systemName: "rectangle.stack.fill.badge.plus")
+                            .foregroundColor(.accentColor)
+                        Text("Add GoToStop widget to your Home Screen")
+                            .font(.body)
+                    }
+                    
+                    HStack(spacing: 16) {
+                        Image(systemName: "wrench.fill")
+                            .foregroundColor(.accentColor)
+                        Text("Setup your widget in Edit Widget")
+                            .font(.body)
+                    }
+                    
+                    HStack(spacing: 16) {
+                        Image(systemName: "house.fill")
+                            .foregroundColor(.accentColor)
+                        Text("Stay updated, right from your home screen")
+                            .font(.body)
+                    }
                 }
+                .padding(.horizontal, 12)
             }
-            .padding(.horizontal, 12)
+            .padding(32)
+            .padding(.horizontal, 20)
         }
-        .padding(32)
-        .padding(.horizontal, 20)
     }
 }
 

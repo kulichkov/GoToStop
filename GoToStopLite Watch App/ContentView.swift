7cc8419acf2e91aa3c7b08bc37b55efe96596758
Mikhail Kulichkov <mikhail.kulichkov@gmail.com>
2025-03-07 14:41:26 +0100

    Add Apple Watch App target GoToStopLite

---
+++ GoToStopLite Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GoToStopLite Watch App
+//
+//  Created by Mikhail Kulichkov on 07.03.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GoToStopLite Watch App/GoToStopLiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoToStopLiteApp.swift
+//  GoToStopLite Watch App
+//
+//  Created by Mikhail Kulichkov on 07.03.25.
+//
+
+import SwiftUI
+
+@main
+struct GoToStopLite_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

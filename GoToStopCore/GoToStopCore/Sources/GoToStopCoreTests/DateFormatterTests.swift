8da6e7983b3490bb901edd1c08366e5f321f3d66
Mikhail Kulichkov <mikhail.kulichkov@gmail.com>
2025-03-08 16:33:23 +0100

    Add test target for GoToStopCore

---
+++ GoToStopCore/GoToStopCore/Sources/GoToStopCoreTests/DateFormatterTests.swift
@@ -0,0 +1,30 @@
+//
+//  DateFormatterTests.swift
+//  GoToStopCore
+//
+//  Created by Mikhail Kulichkov on 08.03.25.
+//
+
+import Testing
+import Foundation
+import GoToStopCore
+
+struct DateFormatterTests {
+
+    @Test
+    func testServerDateFormatter() async throws {
+        let date0 = ServerDateFormatter.date(date: "2025-03-03", time: "13:21:00")
+        let currentZoneGMTOffset = TimeInterval(TimeZone.current.secondsFromGMT())
+        
+        #expect(date0 == Date(timeIntervalSince1970: 1741008060 - currentZoneGMTOffset))
+        
+        let date1 = ServerDateFormatter.date(date: nil, time: "13:21:00")
+        
+        #expect(date1 == nil)
+        
+        let date2 = ServerDateFormatter.date(date: "2025-03-03", time: nil)
+        
+        #expect(date2 == nil)
+    }
+
+}

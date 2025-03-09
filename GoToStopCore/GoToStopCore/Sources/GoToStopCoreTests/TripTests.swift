31bc5b98718392453678fb310c18a39b4f434d43
Mikhail Kulichkov <mikhail.kulichkov@gmail.com>
2025-03-09 19:22:21 +0100

    Add Trip tests

---
+++ GoToStopCore/GoToStopCore/Sources/GoToStopCoreTests/TripTests.swift
@@ -0,0 +1,39 @@
+//
+//  TripTests.swift
+//  GoToStopCore
+//
+//  Created by Mikhail Kulichkov on 09.03.25.
+//
+
+import Testing
+import Foundation
+@testable
+import GoToStopCore
+import CoreLocation
+
+struct TripTests {
+    
+    @Test
+    func initialization() {
+        let name = UUID().uuidString
+        let direction = UUID().uuidString
+        let category = TransportCategory.unknown
+        let lineId = UUID().uuidString
+        let directionId = UUID().uuidString
+        
+        let trip = Trip(
+            name: name,
+            direction: direction,
+            category: category,
+            lineId: lineId,
+            directionId: directionId
+        )
+        
+        #expect(trip.name == name)
+        #expect(trip.direction == direction)
+        #expect(trip.category == category)
+        #expect(trip.lineId == lineId)
+        #expect(trip.directionId == directionId)
+    }
+    
+}
